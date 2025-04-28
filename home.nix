6e4d12fc053d4006ccd01c2787c00e76c3289a17
mkuritsu <mkuritsuu@gmail.com>
2025-04-28 05:35:32 +0100

    theme fix

--- home.nix
+++ home.nix
@@ -9,6 +9,34 @@
   home.homeDirectory = "/home/kuritsu";
   programs.home-manager.enable = true;
 
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome.gnome-themes-extra;
+    };
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
+
   programs = {
     starship = {
       enable = true;
