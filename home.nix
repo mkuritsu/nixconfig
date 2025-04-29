9a0ac88d5168502a26aca004be129cec312047d6
mkuritsu <mkuritsuu@gmail.com>
2025-04-29 04:22:44 +0100

    added zsh keybinds

--- home.nix
+++ home.nix
@@ -69,6 +69,13 @@
       enableCompletion = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+      initExtra = ''
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
+        bindkey  "^[[H"   beginning-of-line
+        bindkey  "^[[F"   end-of-line
+        bindkey  "^[[3~"  delete-char
+      '';
     };
 
     direnv = {
