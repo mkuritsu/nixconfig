76019e8eb2de65443ec7cde63c7a96876d4771b0
kuritsu <rodrigoalexccorreia@gmail.com>
2025-05-03 01:19:31 +0100

    added rust template

---
+++ templates/rust/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "rust starting template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pkg-config
+            (rust-bin.stable.latest.default.override {
+              extensions = ["rust-src" "clippy" "rust-analyzer"]
+            });
+          ];
+        };
+      }
+    );
+}
