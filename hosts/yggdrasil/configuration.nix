625f739b3940d2117e5a7f36d448a6c75a2282be
mkuritsu <mkuritsuu@gmail.com>
2025-05-06 05:28:27 +0100

    networking changes

--- hosts/yggdrasil/configuration.nix
+++ hosts/yggdrasil/configuration.nix
@@ -41,22 +41,18 @@
   networking = {
     hostName = "yggdrasil";
     firewall.enable = true;
-    useDHCP = false;
-    dhcpcd.enable = false;
-    networkmanager.enable = false;
+    defaultGateway = "192.168.1.254";
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    interfaces.enp34s0 = {
+      useDHCP = true;
+      ipv4.addresses = [
+        {
+          address = "192.168.1.70";
+          prefixLength = 24;
+        }
+      ];
+    };
   };
-  networking.interfaces.eth0.ipv4.addresses = [
-    {
-      address = "192.168.1.70";
-      prefixLength = 24;
-    }
-  ];
-  networking.defaultGateway = "192.168.1.254";
-  networking.nameservers = [
-    "1.1.1.1"
-    "1.0.0.1"
-    "8.8.8.8"
-  ];
 
   # Localization
   time.timeZone = "Europe/Lisbon";
