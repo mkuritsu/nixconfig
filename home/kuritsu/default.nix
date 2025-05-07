65b22b5618115720be44b7d2d4e3d01f526ca176
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-05-07 05:26:59 +0100

    modularize and failed attemp to fix osu

--- home/kuritsu/default.nix
+++ home/kuritsu/default.nix
@@ -1,7 +1,4 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   home.stateVersion = "24.11";
 
@@ -12,44 +9,9 @@
     ./hypr
     ./waybar
     ./wofi
+    ./xdg
+    ./gtk
   ];
-
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
-
-  home.pointerCursor = {
-    gtk.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 24;
-  };
-
-  gtk = {
-    enable = true;
-    theme = {
-      name = "rose-pine";
-      package = pkgs.rose-pine-gtk-theme;
-    };
-    iconTheme = {
-      package = pkgs.adwaita-icon-theme;
-      name = "Adwaita";
-    };
-    gtk3.bookmarks = [
-      "file:///home/kuritsu/Documents"
-      "file:///home/kuritsu/Music"
-      "file:///home/kuritsu/Pictures"
-      "file:///home/kuritsu/Videos"
-      "file:///home/kuritsu/Downloads"
-    ];
-  };
-
-  xdg.userDirs = {
-    enable = true;
-    createDirectories = true;
-  };
 
   programs = {
     home-manager.enable = true;
