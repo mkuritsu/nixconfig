a408f4427b4abe0105c5fdeb6dd650258fb9ac9a
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-05-15 17:00:58 +0100

    adb firewall port

--- hosts/yggdrasil/configuration.nix
+++ hosts/yggdrasil/configuration.nix
@@ -35,7 +35,16 @@
   # Networking
   networking = {
     hostName = "yggdrasil";
-    firewall.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 
+        5555 # adb
+      ];
+      extraCommands = ''
+        iptables -A nixos-fw -p tcp --source 192.168.1.0/24 -j nixos-fw-accept
+        iptables -A nixos-fw -p udp --source 192.168.1.0/24 -j nixos-fw-accept
+      '';
+    };
     defaultGateway = "192.168.1.254";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
     interfaces.enp34s0 = {
