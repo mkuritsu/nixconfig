68d462acbe58d03362605dfb72d144ffe3db1f90
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-05-17 10:07:28 +0100

    static ip

--- hosts/yggdrasil/configuration.nix
+++ hosts/yggdrasil/configuration.nix
@@ -35,6 +35,7 @@
   # Networking
   networking = {
     hostName = "yggdrasil";
+    useDHCP = false;
     firewall = {
       enable = true;
       allowedTCPPorts = [ 
@@ -44,6 +45,15 @@
         iptables -A nixos-fw -p tcp --source 192.168.1.0/24 -j nixos-fw-accept
         iptables -A nixos-fw -p udp --source 192.168.1.0/24 -j nixos-fw-accept
       '';
+    };
+    defaultGateway = "192.168.1.254";
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    interfaces.enp34s0 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+        address = "192.168.1.70";
+        prefixLength = 24;
+      }];
     };
   };
 
