da63669f9925d7b7452af364f90272d8de3e07e3
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-06-06 03:34:15 +0100

    add chromium

---
+++ home/kuritsu/browser.nix
@@ -0,0 +1,34 @@
+{ inputs, pkgs, ... }:
+{
+  programs.firefox = {
+    enable = true;
+
+    profiles.default = {
+      extensions = {
+        packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+          bitwarden
+          ublock-origin
+          sponsorblock
+          privacy-badger
+          return-youtube-dislikes
+        ];
+      };
+
+      settings = {
+        "extensions.autoDisableScopes" = 0;
+      };
+    };
+  };
+
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "nngceckbapebfimnlniiiahkandclblb" # bitwarden
+      "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # privacy badger
+      "gebbhagfogifgggkldgodflihgfeippi" # return youtube dislike
+      "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsor block
+      "ddkjiahejlhfcafbddmgiahcphecmpfh" # ublock origin lite
+      "kehjfphhkfppnnjhdfhanmehkegdppho" # youtube row fixer
+    ];
+  };
+}
