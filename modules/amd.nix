0f2c7a45b11904da3390ca463e7b218ee66e0850
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-06-07 05:07:58 +0100

    updated amd config

--- modules/amd.nix
+++ modules/amd.nix
@@ -1,14 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  services.xserver.videoDrivers = [
-    "amdgpu"
-  ];
-
-  hardware.graphics.extraPackages = with pkgs; [
-    amdvlk
-  ];
-
-  hardware.graphics.extraPackages32 = with pkgs; [
-    driversi686Linux.amdvlk
-  ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
 }
