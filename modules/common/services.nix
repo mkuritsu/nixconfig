d0623ffa84a7df42864b7073fbf6bfdca56b1d65
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-06-07 16:58:16 +0100

    added plymouth

--- modules/common/services.nix
+++ modules/common/services.nix
@@ -1,7 +1,12 @@
 { ... }:
 {
   virtualisation.docker.enable = true;
+
   services.printing.enable = true;
+
   services.gvfs.enable = true;
+
   hardware.bluetooth.enable = true;
+
+  boot.plymouth.enable = true;
 }
