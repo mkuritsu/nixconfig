0a833ba8003abd6639bf0303cd0f4b44bcf5414e
mkuritsu <mkuritsuu@gmail.com>
2025-06-16 09:45:13 +0100

    ghostty config

---
+++ home/kuritsu/terminal.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      font-size = 11;
+      font-famility = "jetbrains mono nf";
+      theme = "catppuccin-mocha";
+      background-opacity = 0.95;
+    };
+  };
+}
