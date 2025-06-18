9d8b83885af1e51c3f270e1c5aac90acac8269a0
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-06-18 19:49:18 +0100

    fixing broken thingys

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   description = "mkuritsu's nixconfig";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     aagl = {
-      url = "github:ezKEa/aagl-gtk-on-nix";
+      url = "github:ezKEa/aagl-gtk-on-nix/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     firefox-addons = {
@@ -66,7 +67,7 @@
               {
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
+                home-manager.backupFileExtension = "backup";
                 home-manager.extraSpecialArgs = { inherit inputs; };
                 home-manager.users = userHomes;
               }
--- home/kuritsu/default.nix
+++ home/kuritsu/default.nix
@@ -9,11 +9,9 @@
     ./hypr
     ./browser.nix
     ./git.nix
-    ./gtk.nix
     ./quickshell.nix
     ./shell.nix
     ./spicetify.nix
-    ./terminal.nix
     ./tmux.nix
     ./xdg.nix
   ];
