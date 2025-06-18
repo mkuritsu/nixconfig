761a811941c5077f1fa11a522af6faef2c4af825
mkuritsu <mkuritsuu@gmail.com>
2025-06-18 20:38:17 +0100

    more anime launchers

--- modules/gaming/aagl.nix
+++ modules/gaming/aagl.nix
@@ -6,6 +6,8 @@
 
   programs = {
     honkers-railway-launcher.enable = true;
+    anime-game-launcher.enable = true;
+    sleepy-launcher.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
