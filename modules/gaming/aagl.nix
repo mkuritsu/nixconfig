cd6c9e7b91d60d4efa03792769785835c7ad50b4
mkuritsu <mkuritsuu@gmail.com>
2025-07-13 15:18:10 +0100

    changed anime-game-launcher name

--- modules/gaming/aagl.nix
+++ modules/gaming/aagl.nix
@@ -6,7 +6,7 @@
 
   programs = {
     honkers-railway-launcher.enable = true;
-    an-anime-game-launcher.enable = true;
+    anime-game-launcher.enable = true;
     sleepy-launcher.enable = true;
   };
 
