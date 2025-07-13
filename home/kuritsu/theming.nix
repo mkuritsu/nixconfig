3793cefa52edc61c61ea24c27c91ed1c47f55954
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-07-13 12:03:48 +0100

    changed buttons to legacy everforest

--- home/kuritsu/theming.nix
+++ home/kuritsu/theming.nix
@@ -31,7 +31,7 @@ in
   gtk = {
     enable = true;
     theme = {
-      name = "Everforest-Dark-BL";
+      name = "Everforest-Dark-BL-LB";
       package = pkgs.everforest-gtk-theme;
     };
     iconTheme = {
