02f812222accf6b1b47249d7bf2b7c126b7cefec
mkuritsu <mkuritsuu@gmail.com>
2025-07-13 16:03:28 +0100

    moved templates to other file

---
+++ templates/default.nix
@@ -0,0 +1,26 @@
+{
+  rust = {
+    description = "Rust starting template";
+    path = ./rust;
+  };
+
+  java = {
+    description = "Java starting template";
+    path = ./java;
+  };
+
+  c-cpp = {
+    description = "C/C++ starting template";
+    path = ./c-cpp;
+  };
+
+  python = {
+    description = "Python starting template";
+    path = ./python;
+  };
+
+  minimal = {
+    description = "Minimal starting template for other things";
+    path = ./minimal;
+  };
+}
