9010222b097c1381169f30467415ac46ce776379
mkuritsu <mkuritsuu@gmail.com>
2025-07-14 19:42:18 +0100

    reorganized hosts configurations

--- hosts/zaphkiel/configuration.nix
+++ hosts/zaphkiel/configuration.nix
@@ -7,23 +7,7 @@
   # Networking
   networking = {
     hostName = "zaphkiel";
-    useDHCP = false;
-    dhcpcd.enable = false;
     useNetworkd = true;
-    nameservers = [
-      "1.1.1.1"
-      "1.0.0.1"
-      "8.8.8.8"
-      "8.8.4.4"
-      "2606:4700:4700::1111"
-      "2606:4700:4700::1001"
-      "2001:4860:4860::8888"
-      "2001:4860:4860::8844"
-    ];
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [ ];
-    };
     interfaces.enp34s0 = {
       ipv4.addresses = [
         {
