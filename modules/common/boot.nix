da8a3373edb36a7ab9b12ba435b27032477d57db
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-07-15 09:19:42 +0100

    enabled firmware

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -3,4 +3,6 @@
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  hardware.enableRedistributableFirmware = true;
 }
