a29625a3c0185a38d802ca1d5fb93d96b17b1829
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-07-15 11:48:33 +0100

    moved hyprland to UWSM

--- modules/graphical/hyprland.nix
+++ modules/graphical/hyprland.nix
@@ -2,14 +2,10 @@
 {
   programs.hyprland = {
     enable = true;
-    xwayland.enable = true;
+    withUWSM = true;
   };
   services.hypridle.enable = true;
   programs.hyprlock.enable = true;
-
-  # fix dolphin not showing apps
-  environment.etc."/xdg/menus/applications.menu".text =
-    builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 
   environment.systemPackages = with pkgs; [
     grim
