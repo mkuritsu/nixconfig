50aed3ac98d97682c4ae3cb6d87b35239b2e9625
mkuritsu <mkuritsuu@gmail.com>
2025-07-15 12:29:33 +0100

    replaced ly with sddm because ly doesn't work with UWSM

--- modules/graphical/services.nix
+++ modules/graphical/services.nix
@@ -1,14 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 {
   hardware.opentabletdriver.enable = true;
 
   services.upower.enable = true;
   services.udisks2.enable = true;
-  services.displayManager.ly = {
+
+  services.displayManager.sddm = {
     enable = true;
-    settings = {
-      session_log = ".ly-session.log";
-    };
+    wayland.enable = true;
+    theme = "sddm-astronaut-theme";
+    package = pkgs.kdePackages.sddm;
+    extraPackages = with pkgs; [
+      (sddm-astronaut.override {
+        embeddedTheme = "black_hole";
+      })
+    ];
   };
 
   virtualisation.virtualbox.host.enable = true;
