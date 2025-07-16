4c756440a3f0653867af3e017d237bc106107a53
mkuritsu <mkuritsuu@gmail.com>
2025-07-16 14:37:46 +0100

    aagl keys

--- modules/gaming/programs.nix
+++ modules/gaming/programs.nix
@@ -10,7 +10,7 @@
   };
 
   environment.systemPackages = with pkgs; [
-    prismlauncher
+    # prismlauncher
     (inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin.override {
       pipewire_latency = "128/48000"; # default value causes audio cracks for me
     })
