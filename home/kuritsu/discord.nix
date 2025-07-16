4edd85991b1864429b9e4349e12f9f68395481bc
mkuritsu <mkuritsuu@gmail.com>
2025-07-16 07:52:30 +0100

    removed broken vesktop

--- home/kuritsu/discord.nix
+++ home/kuritsu/discord.nix
@@ -1,8 +1,6 @@
 { ... }:
 {
   home.file = {
-    ".config/vesktop/themes/deshitiffy.css".source = ./discord/themes/deshitiffy.css;
-    ".config/vesktop/themes/Everforest.css".source = ./discord/themes/Everforest.css;
     ".config/Vencord/themes/deshitiffy.css".source = ./discord/themes/deshitiffy.css;
     ".config/Vencord/themes/Everforest.css".source = ./discord/themes/Everforest.css;
   };
