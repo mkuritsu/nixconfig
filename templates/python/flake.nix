156519c77e06e74f500b7236d9de61d6a12b7445
mkuritsu <mkuritsuu@gmail.com>
2025-07-20 13:05:39 +0100

    updated templates to remove flake-utils

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -1,24 +1,35 @@
 {
+  description = "rust flake template";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
+    { nixpkgs, systems, ... }:
+    let
+      eachSystem =
+        fn:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          in
+          fn pkgs
+        );
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
           packages = with pkgs; [
             python3.withPackages
             (pypkgs: [
             ])
           ];
         };
-      }
-    );
+      });
+    };
 }
--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,22 +1,29 @@
 {
+  description = "rust flake template";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
+    { nixpkgs, systems, ... }:
+    let
+      eachSystem =
+        fn:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          in
+          fn pkgs
+        );
+    in
     {
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
           packages = with pkgs; [
             cargo
             clippy
@@ -26,6 +33,6 @@
 
           env.RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
         };
-      }
-    );
+      });
+    };
 }
