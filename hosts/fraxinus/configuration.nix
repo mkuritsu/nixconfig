9506ae79950de827491459854ed608bb386a256c
mkuritsu <mkuritsuu@gmail.com>
2025-07-22 18:14:00 +0100

    fraxinus webhook_url replaced with webhookFile

--- hosts/fraxinus/configuration.nix
+++ hosts/fraxinus/configuration.nix
@@ -50,9 +50,7 @@
     enable = true;
     openFirewall = true;
     username = "Penix IST";
-    # agenix says to not do this but this isn't a sensitive secret, its only encrypted to avoid potential spam in the discord channel by unauthorized parties
-    # since my nxiconfig is public in github, with that this getting put in plain text inside the nix store is fine
-    webhook_url = lib.fileContents config.age.secrets.istannouncements-webhook.path;
+    webhookFile = config.age.secrets.istannouncements-webhook.path;
   };
 
   system.stateVersion = "25.11";
