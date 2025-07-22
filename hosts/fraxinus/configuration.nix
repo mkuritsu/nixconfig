97d7ac16725b8f16a05034f096d6311d13f4c048
mkuritsu <mkuritsuu@gmail.com>
2025-07-22 15:27:19 +0100

    enabled istannouncements in fraxinus and put webhook url as secret

--- hosts/fraxinus/configuration.nix
+++ hosts/fraxinus/configuration.nix
@@ -1,11 +1,13 @@
 {
-  pkgs,
+  config,
+  inputs,
   ...
 }:
 
 {
   imports = [
     ./hardware-configuration.nix
+    inputs.istannouncements.nixosModules.default
   ];
 
   boot.loader.grub.enable = false;
@@ -31,6 +33,15 @@
   services.openssh = {
     enable = true;
     openFirewall = true;
+  };
+
+  services.istannouncements = {
+    enable = true;
+    openFirewall = true;
+    username = "Penix IST";
+    # agenix says to not do this but this isn't a sensitive secret, its only encrypted to avoid potential spam in the discord channel by unauthorized parties
+    # since my nxiconfig is public in github, with that this getting put in plain text inside the nix store is fine
+    # webhook_url = builtins.readFile config.age.secrets.istannouncements-webhook.path;
   };
 
   system.stateVersion = "25.11";
