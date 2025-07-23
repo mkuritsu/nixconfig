0311ffcef8b643a02f99e73632ae5f87e7faa0e1
mkuritsu <mkuritsuu@gmail.com>
2025-07-23 22:12:22 +0100

    changed discord to use the whole themes as home file source

--- home/kuritsu/discord.nix
+++ home/kuritsu/discord.nix
@@ -1,9 +1,5 @@
 { ... }:
 {
-  home.file = {
-    ".config/Vencord/themes/deshitiffy.css".source = ./discord/deshitiffy.css;
-    ".config/Vencord/themes/Everforest.css".source = ./discord/Everforest.css;
-    ".config/Vencord/themes/Tokyonight.css".source = ./discord/Tokyonight.css;
-    ".config/Vencord/settings/settings.json".source = ./discord/settings.json;
-  };
+  home.file.".config/Vencord/themes".source = ./discord/themes;
+  home.file.".config/Vencord/settings/settings.json".source = ./discord/settings.json;
 }
