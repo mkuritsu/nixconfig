95e1298465acceaab192577df6797d93a432ef2c
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-07-28 06:39:04 +0100

    switched terminal to kitty

---
+++ home/kuritsu/kitty.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableZshIntegration = true;
+    font = {
+      name = "CaskaydiaMono Nerd Font Mono";
+      size = 11;
+    };
+    settings.background_opacity = 0.95;
+    themeFile = "tokyo_night_night";
+    keybindings = {
+      "ctrl+shift+enter" = "new_window_with_cwd";
+    };
+  };
+}
