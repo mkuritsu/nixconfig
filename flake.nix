b176052c8142d128ea617edc95953c0f45ed5665
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-07-28 18:16:27 +0100

    replaced NUR firefox-addons with builtin firefox policies

--- flake.nix
+++ flake.nix
@@ -12,10 +12,6 @@
       url = "github:ezKEa/aagl-gtk-on-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
--- home/kuritsu/browser.nix
+++ home/kuritsu/browser.nix
@@ -1,21 +1,31 @@
-{ inputs, pkgs, ... }:
+{ ... }:
 {
   programs.firefox = {
     enable = true;
 
-    profiles.default = {
-      extensions = {
-        packages = with inputs.firefox-addons.packages.${pkgs.system}; [
-          bitwarden
-          ublock-origin
-          sponsorblock
-          privacy-badger
-          return-youtube-dislikes
-        ];
-      };
-
-      settings = {
-        "extensions.autoDisableScopes" = 0; # needed this so extensions actually get installed
+    policies = {
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          "installation_mode" = "force_installed";
+          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        };
+        # bitwarden
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          "installation_mode" = "force_installed";
+          "install_url" =
+            "https://addons.mozilla.org/firefox/downloads/latest/%7B446900e4-71c2-419f-a6a7-df9c091e268b%7D/latest.xpi";
+        };
+        "sponsorBlocker@ajay.app" = {
+          "installation_mode" = "force_installed";
+          "install_url" =
+            "https://addons.mozilla.org/firefox/downloads/latest/sponsorBlocker@ajay.app/latest.xpi";
+        };
+        # return youtube dislike
+        "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+          "installation_mode" = "force_installed";
+          "install_url" =
+            "https://addons.mozilla.org/firefox/downloads/latest/{762f9885-5a13-4abd-9c77-433dcd38b8fd}/latest.xpi";
+        };
       };
     };
   };
@@ -25,7 +35,6 @@
 
     extensions = [
       "nngceckbapebfimnlniiiahkandclblb" # bitwarden
-      "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # privacy badger
       "gebbhagfogifgggkldgodflihgfeippi" # return youtube dislike
       "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsor block
       "ddkjiahejlhfcafbddmgiahcphecmpfh" # ublock origin lite
