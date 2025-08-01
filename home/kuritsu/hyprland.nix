4f3b1d5da28bd10cdc5176f9daf96e1027d7dcb9
mkuritsu <mkuritsuu@gmail.com>
2025-08-01 17:24:07 +0100

    add hypridle config

--- home/kuritsu/hyprland.nix
+++ home/kuritsu/hyprland.nix
@@ -1,5 +1,6 @@
 {...}: {
   home.file.".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
   home.file.".config/hypr/colors.conf".source = ./hypr/tokyonight.conf;
+  home.file.".config/hypr/hypridle.conf".source = ./hypr/hypridle.conf;
   home.file.".config/uwsm/env".source = ./uwsm/env;
 }
