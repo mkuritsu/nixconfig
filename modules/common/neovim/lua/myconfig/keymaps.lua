e9c0db2b9c219cccddd5c81f1b3c846ecabc981c
mkuritsu <mkuritsuu@gmail.com>
2025-08-04 06:48:48 +0100

    neovim: add keybindins

--- modules/common/neovim/lua/myconfig/keymaps.lua
+++ modules/common/neovim/lua/myconfig/keymaps.lua
@@ -3,12 +3,20 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+-- arrow keys
+vim.keymap.set("i", "<M-h>", "<left>")
+vim.keymap.set("i", "<M-j>", "<down>")
+vim.keymap.set("i", "<M-k>", "<up>")
+vim.keymap.set("i", "<M-l>", "<right>")
+
 -- terminal
 vim.keymap.set({ "n", "t" }, "<C-`>", "<cmd>ToggleTerm<CR>")
+vim.keymap.set("t", "<esc>", "<C-\\><C-n>")
 
 -- telescope
 vim.keymap.set("n", "<leader>f", require("telescope.builtin").find_files)
-vim.keymap.set("n", "<leader>tg", require("telescope.builtin").live_grep)
+vim.keymap.set("n", "<leader>g", require("telescope.builtin").live_grep)
+vim.keymap.set("n", "<leader>th", require("telescope.builtin").builtin)
 
 -- file tree
 vim.keymap.set({ "n", "i" }, "<C-b>", "<cmd>Neotree focus<CR>")
