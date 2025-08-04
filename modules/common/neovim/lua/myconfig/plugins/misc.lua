1cd40dc4f1219dbea63efc3b52e01e413d263fe2
mkuritsu <mkuritsuu@gmail.com>
2025-08-04 03:38:47 +0100

    neovim: enabled hidden files in neo-tree

--- modules/common/neovim/lua/myconfig/plugins/misc.lua
+++ modules/common/neovim/lua/myconfig/plugins/misc.lua
@@ -1,16 +1,28 @@
 require("tokyonight").setup {
-  transparent = true,
+  transparent = true
 }
 vim.cmd.colorscheme("tokyonight")
 
-require("lazydev").setup()
+require("lazydev").setup {}
+require("nvim-autopairs").setup {}
+require("toggleterm").setup {}
+require("lualine").setup {}
+
 require("neocord").setup({
   logo_tooltip = "The superior text editor",
 })
-require("nvim-autopairs").setup()
-require("toggleterm").setup()
-require("lualine").setup {}
-require("neo-tree").setup({})
+
+require("neo-tree").setup {
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignore = false,
+      hide_hidden = false
+    },
+    group_empty_dirs = true -- to have a same java development experience with 30 nested directories
+  }
+}
+
 require("which-key").setup {
   preset = "modern"
 }
