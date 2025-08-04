dd66f75dd8ff204ac639e5a315dbdf2f20021f6e
mkuritsu <mkuritsuu@gmail.com>
2025-08-04 04:39:08 +0100

    neovim: add web-devicons

--- modules/common/neovim/lua/myconfig/plugins/misc.lua
+++ modules/common/neovim/lua/myconfig/plugins/misc.lua
@@ -7,6 +7,7 @@ require("lazydev").setup {}
 require("nvim-autopairs").setup {}
 require("toggleterm").setup {}
 require("lualine").setup {}
+require('nvim-web-devicons').setup {}
 
 require("neocord").setup({
   logo_tooltip = "The superior text editor",
