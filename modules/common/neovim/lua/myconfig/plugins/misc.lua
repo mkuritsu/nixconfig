c34e064db8cb58047511436ff310477560e64074
mkuritsu <mkuritsuu@gmail.com>
2025-08-04 10:43:33 +0100

    neovim: autoclose file tree when opening file

--- modules/common/neovim/lua/myconfig/plugins/misc.lua
+++ modules/common/neovim/lua/myconfig/plugins/misc.lua
@@ -13,17 +13,6 @@ require("neocord").setup({
   logo_tooltip = "The superior text editor",
 })
 
-require("neo-tree").setup {
-  filesystem = {
-    filtered_items = {
-      hide_dotfiles = false,
-      hide_gitignore = false,
-      hide_hidden = false
-    },
-    group_empty_dirs = true -- to have a same java development experience with 30 nested directories
-  }
-}
-
 require("which-key").setup {
   preset = "modern"
 }
