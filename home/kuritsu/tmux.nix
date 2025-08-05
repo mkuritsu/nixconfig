bced7ca2d03c08390cd4bd08d2aedbf6090362d6
mkuritsu <mkuritsuu@gmail.com>
2025-08-05 21:48:03 +0100

    tmux: tokyo night theme

--- home/kuritsu/tmux.nix
+++ home/kuritsu/tmux.nix
@@ -1,11 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     mouse = true;
-    newSession = true;
-    prefix = "C-Space";
+    prefix = "C-a";
     baseIndex = 1;
     secureSocket = true;
+    plugins = [
+      {
+        plugin = pkgs.tmuxPlugins.tokyo-night-tmux;
+        extraConfig = "set -g @tokyo-night-tmux_transparent 1";
+      }
+    ];
     extraConfig = ''
       bind-key & kill-window
       bind-key x kill-pane
