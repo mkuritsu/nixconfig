f789a6186b6f9db964d43ad12be57886c78c5c84
mkuritsu <mkuritsuu@gmail.com>
2025-08-06 11:23:22 +0100

    fraxinus: enabled blocky with block list

--- hosts/fraxinus/configuration.nix
+++ hosts/fraxinus/configuration.nix
@@ -56,7 +56,30 @@
     authKeyFile = config.age.secrets.tailscale.path;
   };
 
-  networking.firewall.allowedTCPPorts = [8123 53];
+  # https://wiki.nixos.org/wiki/Blocky
+  services.blocky = {
+    enable = true;
+    settings = {
+      ports.dns = 53;
+      upstreams.groups.default = [
+        "https://one.one.one.one/dns-query"
+      ];
+      bootstrapDns = {
+        upstream = "https://one.one.one.one/dns-query";
+        ips = ["1.1.1.1" "1.0.0.1"];
+      };
+      blocking = {
+        denylists = {
+          ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+        };
+        clientGroupsBlock = {
+          default = ["ads"];
+        };
+      };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [53];
+  networking.firewall.allowedUDPPorts = [53];
 
   system.stateVersion = "25.11";
 }
