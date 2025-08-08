18295fd208dcc7407db29f745a45b2d97914101a
mkuritsu <mkuritsuu@gmail.com>
2025-08-08 20:02:34 +0100

    add flatpak

--- modules/graphical/services.nix
+++ modules/graphical/services.nix
@@ -12,6 +12,14 @@
   services.udisks2.enable = true;
   services.printing.enable = true;
   services.playerctld.enable = true;
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = ["multi-user.target"];
+    path = [pkgs.flatpak];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 
   # https://ryjelsum.me/homelab/greetd-session-choose/
   services.greetd = {
