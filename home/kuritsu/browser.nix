5280b5ca9580214aaa0e3a68b1c64d2ce76dc03f
mkuritsu <mkuritsuu@gmail.com>
2025-08-08 10:15:29 +0100

    firefox: add vimium, refactored way of adding extensions

--- home/kuritsu/browser.nix
+++ home/kuritsu/browser.nix
@@ -1,28 +1,25 @@
-{...}: {
+{...}: let
+  mkFirefoxExtensions = extensions:
+    builtins.listToAttrs (map (extensionId: {
+        name = extensionId;
+        value = {
+          "installation_mode" = "force_installed";
+          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/${extensionId}/latest.xpi";
+        };
+      })
+      extensions);
+in {
   programs.firefox = {
     enable = true;
 
     policies = {
-      ExtensionSettings = {
-        "uBlock0@raymondhill.net" = {
-          "installation_mode" = "force_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        };
-        # bitwarden
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          "installation_mode" = "force_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/%7B446900e4-71c2-419f-a6a7-df9c091e268b%7D/latest.xpi";
-        };
-        "sponsorBlocker@ajay.app" = {
-          "installation_mode" = "force_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/sponsorBlocker@ajay.app/latest.xpi";
-        };
-        # return youtube dislike
-        "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
-          "installation_mode" = "force_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/{762f9885-5a13-4abd-9c77-433dcd38b8fd}/latest.xpi";
-        };
-      };
+      ExtensionSettings = mkFirefoxExtensions [
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" #bitwarden
+        "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" # return youtube dislike
+        "sponsorBlocker@ajay.app"
+        "uBlock0@raymondhill.net"
+        "{d7742d87-e61d-4b78-b8a1-b469842139fa}" # vimium
+      ];
     };
   };
 
