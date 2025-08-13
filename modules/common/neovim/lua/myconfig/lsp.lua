4e76de2745cf224282d78b321a2782ee9675805b
mkuritsu <mkuritsuu@gmail.com>
2025-08-13 19:22:48 +0100

    neovim: disabled cmake & markdown auto formatting

--- modules/common/neovim/lua/myconfig/lsp.lua
+++ modules/common/neovim/lua/myconfig/lsp.lua
@@ -1,12 +1,16 @@
-vim.lsp.config("nixd", {
-  settings = {
-    nixd = {
-      formatting = {
-        command = { "alejandra", "--quiet" }
+local function set_lsp_formatter(lsp, command)
+  vim.lsp.config(lsp, {
+    settings = {
+      [lsp] = {
+        formatting = {
+          command = command
+        }
       }
     }
-  }
-})
+  })
+end
+
+set_lsp_formatter("nixd", { "alejandra", "--quiet" })
 
 vim.lsp.enable({
   "lua_ls", "ts_ls", "clangd", "pyright", "qmlls", "rust_analyzer", "jdtls", "marksman", "html", "cssls", "astro",
@@ -14,12 +18,24 @@ vim.lsp.enable({
 })
 
 -- auto format on save
+local no_format_list = { "cmake", "marksman" }
+
+local function should_format(client)
+  for _, lsp in ipairs(no_format_list) do
+    if client.name == lsp then
+      return false
+    end
+  end
+  return true
+end
+
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
     if not client:supports_method('textDocument/willSaveWaitUntil')
-        and client:supports_method('textDocument/formatting') then
+        and client:supports_method('textDocument/formatting')
+        and should_format(client) then
       vim.api.nvim_create_autocmd('BufWritePre', {
         group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
         buffer = args.buf,
