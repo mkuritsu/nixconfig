5d691945beb5911630f61cfc8692c4809131a39a
mkuritsu <mkuritsuu@gmail.com>
2025-08-17 15:23:42 +0100

    not a single gtk theme works flawlessy

--- home/kuritsu/theming.nix
+++ home/kuritsu/theming.nix
@@ -22,9 +22,9 @@
 
   gtk = {
     enable = true;
-    theme = {
-      name = "rose-pine";
-      package = pkgs.rose-pine-gtk-theme;
+    gtk3.theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
     };
     iconTheme = {
       package = pkgs.adwaita-icon-theme;
