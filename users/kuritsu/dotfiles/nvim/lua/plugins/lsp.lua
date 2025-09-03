55c09997702052cebb41a2745d470e7992b262bb
mkuritsu <mkuritsuu@gmail.com>
2025-09-03 09:16:29 +0100

    changed python lsp

--- users/kuritsu/dotfiles/nvim/lua/plugins/lsp.lua
+++ users/kuritsu/dotfiles/nvim/lua/plugins/lsp.lua
@@ -4,8 +4,8 @@ return {
     event = "VeryLazy",
     config = function()
       vim.lsp.enable({
-        "lua_ls", "ts_ls", "clangd", "pyright", "qmlls", "rust_analyzer", "jdtls", "marksman", "html", "cssls", "astro",
-        "nixd", "cmake", "ruff"
+        "lua_ls", "ts_ls", "clangd", "basedpyright", "qmlls", "rust_analyzer", "jdtls", "marksman", "html", "cssls",
+        "astro", "nixd", "cmake", "ruff"
       })
     end
   },
