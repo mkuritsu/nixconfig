ffba91721c01c7333161e19cc2217a7c88af2fea
mkuritsu <mkuritsuu@gmail.com>
2025-09-13 13:07:38 +0100

    add devtools

---
+++ modules/graphical/devtools.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    cargo
+    rustc
+    clang
+    gcc
+    cmake
+    ninja
+    meson
+    jdk21
+    maven
+    python3
+    nodejs
+    pnpm
+    dotnet-sdk
+    pkg-config
+    uv
+  ];
+}
