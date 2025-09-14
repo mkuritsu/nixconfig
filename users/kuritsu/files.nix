032e4a569eaae17db1b4612977a716ca3ffc89d7
mkuritsu <mkuritsuu@gmail.com>
2025-09-15 00:13:25 +0100

    symlink optional

--- users/kuritsu/files.nix
+++ users/kuritsu/files.nix
@@ -1,50 +1,51 @@
+useSymlinks:
 { config, ... }:
 let
   inherit (config.lib.file) mkOutOfStoreSymlink;
+  sourceFile = path: if useSymlinks then mkOutOfStoreSymlink path else path;
 in
 {
   xdg.configFile = {
-    "Kvantum/Tokyonight".source = mkOutOfStoreSymlink ./dotfiles/Kvantum/Tokyonight;
-    "Kvantum/kvantum.kvconfig".source = mkOutOfStoreSymlink ./dotfiles/Kvantum/kvantum.kvconfig;
-    "qt6ct/qt6ct.conf".source = mkOutOfStoreSymlink ./dotfiles/qt6ct/qt6ct.conf;
+    "Kvantum/Tokyonight".source = sourceFile ./dotfiles/Kvantum/Tokyonight;
+    "Kvantum/kvantum.kvconfig".source = sourceFile ./dotfiles/Kvantum/kvantum.kvconfig;
+    "qt6ct/qt6ct.conf".source = sourceFile ./dotfiles/qt6ct/qt6ct.conf;
 
-    "kdeglobals".source = mkOutOfStoreSymlink ./dotfiles/kdeglobals;
+    "kdeglobals".source = sourceFile ./dotfiles/kdeglobals;
 
-    "mako/config".source = mkOutOfStoreSymlink ./dotfiles/mako/config;
+    "mako/config".source = sourceFile ./dotfiles/mako/config;
 
-    "hypr/hyprland.conf".source = mkOutOfStoreSymlink ./dotfiles/hypr/hyprland.conf;
-    "hypr/hypridle.conf".source = mkOutOfStoreSymlink ./dotfiles/hypr/hypridle.conf;
-    "uwsm/env".source = mkOutOfStoreSymlink ./dotfiles/uwsm/env;
+    "hypr/hyprland.conf".source = sourceFile ./dotfiles/hypr/hyprland.conf;
+    "hypr/hypridle.conf".source = sourceFile ./dotfiles/hypr/hypridle.conf;
+    "uwsm/env".source = sourceFile ./dotfiles/uwsm/env;
 
-    "fuzzel/fuzzel.ini".source = mkOutOfStoreSymlink ./dotfiles/fuzzel/fuzzel.ini;
+    "fuzzel/fuzzel.ini".source = sourceFile ./dotfiles/fuzzel/fuzzel.ini;
 
-    "imv/config".source = mkOutOfStoreSymlink ./dotfiles/imv/config;
+    "imv/config".source = sourceFile ./dotfiles/imv/config;
 
-    "niri/config.kdl".source = mkOutOfStoreSymlink ./dotfiles/niri/config.kdl;
+    "niri/config.kdl".source = sourceFile ./dotfiles/niri/config.kdl;
 
-    "btop/btop.conf".source = mkOutOfStoreSymlink ./dotfiles/btop/btop.conf;
-    "btop/themes".source = mkOutOfStoreSymlink ./dotfiles/btop/themes;
+    "btop/btop.conf".source = sourceFile ./dotfiles/btop/btop.conf;
+    "btop/themes".source = sourceFile ./dotfiles/btop/themes;
 
-    "git/config".source = mkOutOfStoreSymlink ./dotfiles/git/config;
+    "git/config".source = sourceFile ./dotfiles/git/config;
 
-    "tmux/tmux.conf".source = mkOutOfStoreSymlink ./dotfiles/tmux/tmux.conf;
+    "tmux/tmux.conf".source = sourceFile ./dotfiles/tmux/tmux.conf;
 
-    "starship.toml".source = mkOutOfStoreSymlink ./dotfiles/starship.toml;
+    "starship.toml".source = sourceFile ./dotfiles/starship.toml;
 
-    "kitty/kitty.conf".source = mkOutOfStoreSymlink ./dotfiles/kitty/kitty.conf;
-    "kitty/themes".source = mkOutOfStoreSymlink ./dotfiles/kitty/themes;
+    "kitty/kitty.conf".source = sourceFile ./dotfiles/kitty/kitty.conf;
+    "kitty/themes".source = sourceFile ./dotfiles/kitty/themes;
 
-    "fish/config.fish".source = mkOutOfStoreSymlink ./dotfiles/fish/config.fish;
-    "fish/functions/cd_fzf.fish".source = mkOutOfStoreSymlink ./dotfiles/fish/functions/cd_fzf.fish;
+    "fish/config.fish".source = sourceFile ./dotfiles/fish/config.fish;
+    "fish/functions/cd_fzf.fish".source = sourceFile ./dotfiles/fish/functions/cd_fzf.fish;
     "fish/functions/fish_user_key_bindings.fish".source =
       ./dotfiles/fish/functions/fish_user_key_bindings.fish;
     "fish/functions/fish_greeting.fish".source =
-      mkOutOfStoreSymlink ./dotfiles/fish/functions/fish_greeting.fish;
+      sourceFile ./dotfiles/fish/functions/fish_greeting.fish;
 
-    "nvim".source = mkOutOfStoreSymlink ./dotfiles/nvim;
+    "nvim".source = sourceFile ./dotfiles/nvim;
 
     "xdg-desktop-portal/Hyprland-portals.conf".source =
       ./dotfiles/xdg-desktop-portal/Hyprland-portals.conf;
   };
-
 }
