447dae2532d22c497bf6f8b7f34e4c11e22cef56
mkuritsu <mkuritsuu@gmail.com>
2025-09-15 20:31:43 +0100

    neovim format

--- users/kuritsu/dotfiles/nvim/lua/config/misc.lua
+++ users/kuritsu/dotfiles/nvim/lua/config/misc.lua
@@ -4,20 +4,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-    if not client:supports_method('textDocument/willSaveWaitUntil')
-        and client:supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-        end,
-      })
-    end
-  end
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    vim.lsp.buf.format({ timeout_ms = 1000 })
+  end,
 })
 
 vim.diagnostic.config({ virtual_text = true })
