a9c6c3d03870751bee5b3244616ca718978fc27f
mkuritsu <mkuritsuu@gmail.com>
2025-09-19 20:20:53 +0100

    gnome packages changes

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,4 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = with pkgs; [
+    totem
+    evince
+    rhythmbox
+    gnome-music
+    geary
+  ];
+
+  environment.systemPackages = with pkgs; [
+    showtime
+    papers
+    amberol
+  ];
 }
