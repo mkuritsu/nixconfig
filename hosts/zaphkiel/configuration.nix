65228db569df65e5cf3a06b42125ac207ae521e1
mkuritsu <mkuritsuu@gmail.com>
2025-09-24 02:27:39 +0100

    removed systems

--- hosts/zaphkiel/configuration.nix
+++ hosts/zaphkiel/configuration.nix
@@ -8,21 +8,19 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Networking
   networking = {
-    networkmanager.enable = true;
-    # interfaces.enp34s0 = {
-    #   ipv4.addresses = [
-    #     {
-    #       address = "192.168.1.70";
-    #       prefixLength = 24;
-    #     }
-    #   ];
-    # };
-    # defaultGateway = {
-    #   address = "192.168.1.254";
-    #   interface = "enp34s0";
-    # };
+    interfaces.enp34s0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.1.70";
+          prefixLength = 24;
+        }
+      ];
+    };
+    defaultGateway = {
+      address = "192.168.1.254";
+      interface = "enp34s0";
+    };
   };
 
   services.openssh = {
