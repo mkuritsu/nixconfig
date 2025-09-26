675d3a304790ac282fd3d96a9d2edcd2567a3d3e
mkuritsu <mkuritsuu@gmail.com>
2025-09-26 11:24:17 +0100

    removed iwd backend

--- hosts/camael/configuration.nix
+++ hosts/camael/configuration.nix
@@ -9,10 +9,8 @@
 
   # Networking
   networking = {
-    wireless.iwd.enable = true;
     networkmanager.enable = true;
     networkmanager.dns = "none";
-    networkmanager.wifi.backend = "iwd";
   };
 
   services.thermald.enable = true;
