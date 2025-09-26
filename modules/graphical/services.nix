22cc68bc40c50f516c32e18d8d97a7a9c6ab37a2
mkuritsu <mkuritsuu@gmail.com>
2025-09-26 02:15:11 +0100

    replaced gdm with tuigreet

--- modules/graphical/services.nix
+++ modules/graphical/services.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
   hardware = {
     bluetooth.enable = true;
@@ -15,12 +15,18 @@
     printing.enable = true;
     playerctld.enable = true;
     flatpak.enable = true;
-    displayManager.gdm.enable = true;
-    gnome.gnome-keyring.enable = true;
-    fprintd.enable = true;
   };
 
-  security.pam.services.login.enableGnomeKeyring = true;
+  # https://ryjelsum.me/homelab/greetd-session-choose/
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session --asterisks";
+        user = "greeter";
+      };
+    };
+  };
 
   xdg = {
     mime.enable = true;
