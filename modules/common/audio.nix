98ba57f21734de309997ec88b970222c02743ef6
mkuritsu <mkuritsuu@gmail.com>
2025-10-07 21:41:12 +0100

    increase pipewire quantum

--- modules/common/audio.nix
+++ modules/common/audio.nix
@@ -1,4 +1,5 @@
-{...}: {
+{ ... }:
+{
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -7,5 +8,14 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    extraConfig.pipewire = {
+      "10-clock-quantum" = {
+        "context.properties" = {
+          "default.clock.quantum" = 1024;
+          "default.clock.min-quantum" = 1024;
+          "default.clock.max-quantum" = 1024;
+        };
+      };
+    };
   };
 }
