7808bc8c1795be60996876a56679bf4219442554
mkuritsu <mkuritsuu@gmail.com>
2025-10-10 02:56:00 +0100

    updated switch script

--- switch.sh
+++ switch.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-nh os switch . || exit 1
-NIXPKGS_ALLOW_UNFREE=1 home-manager switch -f users/kuritsu/dotfiles/default.nix -b backup
+FLAKE_ROOT=$(pwd) nh os switch . --impure
