95025fc19249e8d3ce414d01f37a67bd8462f85a
mkuritsu <mkuritsuu@gmail.com>
2025-10-24 19:54:11 +0100

    flake update, browser changes, zaphkiel disk hardware

--- users/kuritsu/modules/browser.nix
+++ users/kuritsu/modules/browser.nix
@@ -19,7 +19,6 @@ in
     policies = {
       ExtensionSettings = mkFirefoxExtensions [
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" # bitwarden
-        "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" # return youtube dislike
         "sponsorBlocker@ajay.app"
         "uBlock0@raymondhill.net"
         "{d7742d87-e61d-4b78-b8a1-b469842139fa}" # vimium
@@ -35,7 +34,6 @@ in
 
     extensions = [
       "nngceckbapebfimnlniiiahkandclblb" # bitwarden
-      "gebbhagfogifgggkldgodflihgfeippi" # return youtube dislike
       "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsor block
       "ddkjiahejlhfcafbddmgiahcphecmpfh" # ublock origin lite
       "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
