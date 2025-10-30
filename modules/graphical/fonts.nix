911d532026ceffe3f7c2f59819834ea6ad61f1d6
mkuritsu <mkuritsuu@gmail.com>
2025-10-30 22:17:04 +0000

    renamed emoji package, add symbols only nerd font

--- modules/graphical/fonts.nix
+++ modules/graphical/fonts.nix
@@ -4,12 +4,13 @@
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
-    noto-fonts-emoji
+    noto-fonts-color-emoji
     dejavu_fonts
     cantarell-fonts
     adwaita-fonts
     nerd-fonts.jetbrains-mono
     nerd-fonts.caskaydia-mono
     nerd-fonts.lilex
+    nerd-fonts.symbols-only
   ];
 }
