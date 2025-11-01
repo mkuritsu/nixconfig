110654c85f0b47d4e6fd6e89519b3028708cf43c
mkuritsu <mkuritsuu@gmail.com>
2025-11-01 04:44:08 +0000

    nvim: removed lazydev

--- users/kuritsu/dots/nvim/lua/plugins/lsp.lua
+++ users/kuritsu/dots/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,10 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-    config = function()
-      vim.lsp.enable({
-        "lua_ls", "ts_ls", "clangd", "basedpyright", "qmlls", "rust_analyzer", "jdtls", "marksman", "html", "cssls",
-        "astro", "nixd", "cmake", "ruff"
-      })
-    end
-  },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        "lazy.nvim",
-      }
-    }
-  }
+  "neovim/nvim-lspconfig",
+  event = "VeryLazy",
+  config = function()
+    vim.lsp.enable({
+      "lua_ls", "ts_ls", "clangd", "basedpyright", "qmlls", "rust_analyzer", "jdtls", "marksman", "html", "cssls",
+      "astro", "nixd", "cmake", "ruff"
+    })
+  end
 }
