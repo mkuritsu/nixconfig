cd85406980797e62abb4bc5e735d7c71f30a1819
mkuritsu <mkuritsuu@gmail.com>
2026-05-02 18:58:11 +0100

    merged editors fhs packages

--- users/kuritsu/modules/dev.nix
+++ users/kuritsu/modules/dev.nix
@@ -7,36 +7,39 @@
 }: let
   inherit (pkgs.stdenv.hostPlatform) system;
 
+  fhs-packages = pkgs: with pkgs; [bash rustup gcc pkg-config wayland libxkbcommon udev udev.dev libinput libgbm seatd seatd.dev libGL libGL.dev tree-sitter nodejs python3];
+
   dev-fhs-env = pkgs.buildFHSEnv {
     name = "dev-fhs-env";
-    targetPkgs = pkgs: (osConfig.environment.systemPackages ++ (with pkgs; [bash rustup gcc pkg-config wayland libxkbcommon udev udev.dev libinput libgbm seatd seatd.dev libGL libGL.dev neovim tree-sitter nodejs python3 vscode zed-editor]));
-    runScript = ''
-      bash -c 'export IN_NIX_SHELL=impure name=dev-fhs-env; $SHELL'
+    targetPkgs = pkgs: [pkgs.vscode pkgs.zed-editor pkgs.neovim] ++ (fhs-packages pkgs);
+    runScript = "fish";
+    profile = ''
+      export IN_NIX_SHELL=impure
+      export name=dev-fhs-env
+    '';
+  };
+
+  neovim-fhs = pkgs.buildFHSEnv {
+    name = "nvim";
+    targetPkgs = pkgs: [pkgs.neovim] ++ (fhs-packages pkgs);
+    runScript = "nvim";
+    profile = ''
+      export IN_NIX_SHELL=impure
+      export name=neovim-fhs
     '';
   };
 
   # HACK: probably find later a better way to check for graphical environment?
   is-graphical = osConfig.hardware.graphics.enable;
 in {
-  programs.distrobox = lib.mkIf is-graphical {
-    enable = true;
-    enableSystemdUnit = true;
-    containers = {
-      arch-dev = {
-        image = "quay.io/toolbx/arch-toolbox:latest";
-        entry = true;
-        additional_packages = "git python ripgrep fzf fd fish starship gcc make clang rustup";
-      };
-    };
-  };
-
   home.packages =
     if is-graphical
     then [
       dev-fhs-env
-      self.packages.${system}.neovim-fhs
-      self.packages.${system}.my-vscode-fhs
-      pkgs.zed-editor-fhs
+      neovim-fhs
+      (pkgs.vscode.fhsWithPackages fhs-packages)
+      (pkgs.zed-editor.fhsWithPackages fhs-packages)
+      pkgs.distrobox
     ]
     else [pkgs.neovim];
 }
