64089bdbfc8a8b4838985c22be47b8f71701be62
mkuritsu <mkuritsuu@gmail.com>
2026-05-07 20:45:44 +0100

    xdg-user-dirs-gtk activation script

--- users/kuritsu/modules/xdg.nix
+++ users/kuritsu/modules/xdg.nix
@@ -1,7 +1,14 @@
-{
+{pkgs, lib, ...}: {
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
     setSessionVariables = true;
+  };
+
+  home.activation = {
+    # Update gtk bookmarks
+    gtk-bookmarks = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      ${lib.getExe pkgs.xdg-user-dirs-gtk}
+    '';
   };
 }
