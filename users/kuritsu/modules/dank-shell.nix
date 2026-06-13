9bbde3567943c3766171cbb6396003c064d8e3a9
mkuritsu <mkuritsuu@gmail.com>
2026-06-13 09:00:54 +0100

    fixed styling

--- users/kuritsu/modules/dank-shell.nix
+++ users/kuritsu/modules/dank-shell.nix
@@ -16,19 +16,34 @@
     name = "Adwaita";
     size = 24;
 
-    gtk.enable = true;
     x11.enable = true;
   };
 
   home.packages = [
     pkgs.kdePackages.qt6ct
+    pkgs.adwaita-icon-theme
   ];
 
   gtk = {
     enable = true;
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+    font = {
+      name = "Adwaita Sans";
+      size = 11;
+      package = pkgs.adwaita-fonts;
+    };
     gtk3.theme = {
       name = "adw-gtk3";
       package = pkgs.adw-gtk3;
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = ":minimize,maximize,close";
     };
   };
 }
